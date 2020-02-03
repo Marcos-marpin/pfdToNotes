ade9c262181c9c56dd1bf929a01aaa895872e1a2
Marcos López Lamas <marpin@MacBook-Pro-de-Marcos.local>
2020-02-03 01:09:00 +0100

    Fixed pdf page size

--- pdfNoteSpaceAdder.py
+++ pdfNoteSpaceAdder.py
@@ -5,12 +5,13 @@ import re
 import sys
 
 SCALE_FACTOR = 0.50
-WIDTH_A4 = 595
-HEIGHT_A4 = 842
+
 
 def crear_pagina_notas(pdf, pagina):
-    pagina_notas = PageObject.createBlankPage(pdf, WIDTH_A4, HEIGHT_A4)
-    ty = HEIGHT_A4 - float(pagina.mediaBox.upperLeft[1]) * SCALE_FACTOR # traslacion en y
+    width = pagina.mediaBox.upperRight[0]
+    height = pagina.mediaBox.upperRight[1]
+    pagina_notas = PageObject.createBlankPage(pdf, width, height)
+    ty = float(height) - float(height) * SCALE_FACTOR  # traslacion en y
     pagina_notas.mergeScaledTranslatedPage(pagina, SCALE_FACTOR, 0, ty)
     return pagina_notas
 
@@ -27,5 +28,5 @@ with open(args.inputPDF, "rb") as (inputPDF), \
     newPfd = PdfFileWriter()
     for i in range(pdf.getNumPages()):
         pagina = pdf.getPage(i)
-        newPfd.addPage(crear_pagina_notas(pdf, paginacom))
+        newPfd.addPage(crear_pagina_notas(pdf, pagina))
     newPfd.write(outputPDF)
